e00c9ba2b2a94bfbb5af2e7a31064afcb048a978
kazi-kabir <kazi.s221999@gmail.com>
2022-12-06 22:23:56 +1100

    added values

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('foobar')
+fetch('http://localhost:3000/founders', {
+    method: 'GET',
+    headers: {
+    },
+})
+.then(response => response.text())
+.then(text => console.log(text))
