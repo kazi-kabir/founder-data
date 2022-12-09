d9523248cce8ed05e672cc7d5e1ad86553cd526c
kazi-kabir <kazi.s221999@gmail.com>
2022-12-09 20:44:12 +1100

    left some comments and added some styling

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let arrayOfData = [];
 
-fetch('http://localhost:3000/companyData', {
-    method: 'GET',
-    headers: {
-    },
-})
-.then(response => response.text())
-.then(text => console.log(text))
+// fetch('http://localhost:3000/companyData', {
+//     method: 'GET',
+//     headers: {
+//     },
+// })
+// .then(response => console.log(response.body))
 
+
+// store JSON data somewhere 
 // get JSON data as an array
+// push that into elements
+
 
